6000d7a6cefb5bd253463c0dc8d17effa49b5412
rajtuts <rajtuts79@gmail.com>
2024-08-12 15:13:23 -0500

    Update 17-01173-immediate-food-delivery-i.sql

--- 17-01173-immediate-food-delivery-i.sql
+++ 17-01173-immediate-food-delivery-i.sql
@@ -1,5 +1,70 @@
--- simple condition in aggregate function- count immediate, divide by total rows in the table
+-- 1173. Immediate Food Delivery I
 
+Table: Delivery
+
++-----------------------------+---------+
+| Column Name                 | Type    |
++-----------------------------+---------+
+| delivery_id                 | int     |
+| customer_id                 | int     |
+| order_date                  | date    |
+| customer_pref_delivery_date | date    |
++-----------------------------+---------+
+delivery_id is the primary key (column with unique values) of this table.
+The table holds information about food delivery to customers that make orders at some date and specify a preferred delivery date (on the same order date or after it).
+
+ 
+If the customer's preferred delivery date is the same as the order date, then the order is called immediate, otherwise, it is called scheduled.
+
+Write a solution to find the percentage of immediate orders in the table, rounded to 2 decimal places.
+
+The result format is in the following example.
+
+
+Example 1:
+
+Input: 
+Delivery table:
++-------------+-------------+------------+-----------------------------+
+| delivery_id | customer_id | order_date | customer_pref_delivery_date |
++-------------+-------------+------------+-----------------------------+
+| 1           | 1           | 2019-08-01 | 2019-08-02                  |
+| 2           | 5           | 2019-08-02 | 2019-08-02                  |
+| 3           | 1           | 2019-08-11 | 2019-08-11                  |
+| 4           | 3           | 2019-08-24 | 2019-08-26                  |
+| 5           | 4           | 2019-08-21 | 2019-08-22                  |
+| 6           | 2           | 2019-08-11 | 2019-08-13                  |
++-------------+-------------+------------+-----------------------------+
+Output: 
++----------------------+
+| immediate_percentage |
++----------------------+
+| 33.33                |
++----------------------+
+Explanation: The orders with delivery id 2 and 3 are immediate while the others are scheduled.
+
+
+-- SQL Schema
+Create table If Not Exists Delivery (delivery_id int, customer_id int, order_date date, customer_pref_delivery_date date)
+Truncate table Delivery
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('1', '1', '2019-08-01', '2019-08-02')
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('2', '5', '2019-08-02', '2019-08-02')
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('3', '1', '2019-08-11', '2019-08-11')
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('4', '3', '2019-08-24', '2019-08-26')
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('5', '4', '2019-08-21', '2019-08-22')
+insert into Delivery (delivery_id, customer_id, order_date, customer_pref_delivery_date) values ('6', '2', '2019-08-11', '2019-08-13')
+
+-- Solution
+-- simple condition in aggregate function- count immediate, divide by total rows in the table
+----------------------------------------------------------------------------------------------------------------
+--Oracle
+----------------------------------------------------------------------------------------------------------------
+select round((select count(*) from delivery where order_date = customer_pref_delivery_date)  * 100/ (select count(*) from delivery), 2) as immediate_percentage
+from dual
+
+----------------------------------------------------------------------------------------------------------------
+-- MySQL
+----------------------------------------------------------------------------------------------------------------
 select round(sum(order_date = customer_pref_delivery_date) / count(*) * 100, 2) as immediate_percentage
 from Delivery
 ---------------------------------------------------------------------------------------------------------------------------------------------
