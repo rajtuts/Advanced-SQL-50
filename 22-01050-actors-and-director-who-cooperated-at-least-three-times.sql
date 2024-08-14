6452af3042d0c128f7430993c4e6810daacf15ac
rajtuts <rajtuts79@gmail.com>
2024-08-13 20:58:39 -0500

    Update 22-01050-actors-and-director-who-cooperated-at-least-three-times.sql

--- 22-01050-actors-and-director-who-cooperated-at-least-three-times.sql
+++ 22-01050-actors-and-director-who-cooperated-at-least-three-times.sql
@@ -1,5 +1,73 @@
--- aggregate- group by 2 columns, count
+-- 1050. Actors and Directors Who Cooperated At Least Three Times
+Table: ActorDirector
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key (column with unique values) for this table.
+
+ 
+
+Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+Output: 
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
+
+-- SQL Schema:
+Create table If Not Exists ActorDirector (actor_id int, director_id int, timestamp int)
+Truncate table ActorDirector
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '0')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '1')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '2')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '3')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '4')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '5')
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '6')
+
+  
+  -- Solution
+  -- aggregate- group by 2 columns, count
+----------------------------------------------------------------------------------------------------------------
+--Oracle
+----------------------------------------------------------------------------------------------------------------
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(*) >= 3
+
+----------------------------------------------------------------------------------------------------------------
+-- MySQL
+----------------------------------------------------------------------------------------------------------------
 select actor_id, director_id
 from ActorDirector
 group by 1, 2
