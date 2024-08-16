bd920751e4ecda8fa40f90e9643357e55c4049f6
rajtuts <rajtuts79@gmail.com>
2024-08-15 22:09:56 -0500

    Update 29-00613-shortest-distance-in-a-line.sql

--- 29-00613-shortest-distance-in-a-line.sql
+++ 29-00613-shortest-distance-in-a-line.sql
@@ -1,3 +1,66 @@
+613. Shortest Distance in a Line
+
+Table: Point
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
++-------------+------+
+In SQL, x is the primary key column for this table.
+Each row of this table indicates the position of a point on the X-axis.
+
+Find the shortest distance between any two points from the Point table.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Point table:
++----+
+| x  |
++----+
+| -1 |
+| 0  |
+| 2  |
++----+
+Output: 
++----------+
+| shortest |
++----------+
+| 1        |
++----------+
+Explanation: The shortest distance is between points -1 and 0 which is |(-1) - 0| = 1.
+
+Follow up: How could you optimize your solution if the Point table is ordered in ascending order?
+
+-- SQL Schema
+Create Table If Not Exists Point (x int not null)
+Truncate table Point
+insert into Point (x) values ('-1')
+insert into Point (x) values ('0')
+insert into Point (x) values ('2')
+
+-- Solution
+----------------------------------------------------------------------------------------------------------------
+--Oracle
+----------------------------------------------------------------------------------------------------------------
+-- 1. using Lead funtion
+select min(diff) shortest
+from(
+select lead(x) over(order by x) -x diff
+from Point
+ )
+
+-- 2. using CTE and ABS function
+
+
+  
+----------------------------------------------------------------------------------------------------------------
+-- MySQL
+----------------------------------------------------------------------------------------------------------------
+
 -- cross joining all the points from 2 tables, except the ones where they are same
 -- find the min of absolute distance
 
