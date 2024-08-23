ae5596d9cba3d545ef09bd4df9ae5f8ef1939570
rajtuts <rajtuts79@gmail.com>
2024-08-23 12:40:51 -0500

    Update 47-01270-all-people-report-to-the-given-manager.sql

--- 47-01270-all-people-report-to-the-given-manager.sql
+++ 47-01270-all-people-report-to-the-given-manager.sql
@@ -1,3 +1,80 @@
+1270. All People Report to the Given Manager
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| employee_id   | int     |
+| employee_name | varchar |
+| manager_id    | int     |
++---------------+---------+
+employee_id is the column of unique values for this table.
+Each row of this table indicates that the employee with ID employee_id and name employee_name reports his work to his/her direct manager with manager_id
+The head of the company is the employee with employee_id = 1.
+
+ 
+
+Write a solution to find employee_id of all employees that directly or indirectly report their work to the head of the company.
+
+The indirect relation between managers will not exceed three managers as the company is small.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+---------------+------------+
+| employee_id | employee_name | manager_id |
++-------------+---------------+------------+
+| 1           | Boss          | 1          |
+| 3           | Alice         | 3          |
+| 2           | Bob           | 1          |
+| 4           | Daniel        | 2          |
+| 7           | Luis          | 4          |
+| 8           | Jhon          | 3          |
+| 9           | Angela        | 8          |
+| 77          | Robert        | 1          |
++-------------+---------------+------------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 2           |
+| 77          |
+| 4           |
+| 7           |
++-------------+
+Explanation: 
+The head of the company is the employee with employee_id 1.
+The employees with employee_id 2 and 77 report their work directly to the head of the company.
+The employee with employee_id 4 reports their work indirectly to the head of the company 4 --> 2 --> 1. 
+The employee with employee_id 7 reports their work indirectly to the head of the company 7 --> 4 --> 2 --> 1.
+The employees with employee_id 3, 8, and 9 do not report their work to the head of the company directly or indirectly. 
+
+-- SQL schema
+Create table If Not Exists Employees (employee_id int, employee_name varchar(30), manager_id int)
+Truncate table Employees
+insert into Employees (employee_id, employee_name, manager_id) values ('1', 'Boss', '1')
+insert into Employees (employee_id, employee_name, manager_id) values ('3', 'Alice', '3')
+insert into Employees (employee_id, employee_name, manager_id) values ('2', 'Bob', '1')
+insert into Employees (employee_id, employee_name, manager_id) values ('4', 'Daniel', '2')
+insert into Employees (employee_id, employee_name, manager_id) values ('7', 'Luis', '4')
+insert into Employees (employee_id, employee_name, manager_id) values ('8', 'John', '3')
+insert into Employees (employee_id, employee_name, manager_id) values ('9', 'Angela', '8')
+insert into Employees (employee_id, employee_name, manager_id) values ('77', 'Robert', '1')
+
+-- Solution
+----------------------------------------------------------------------------------------------------------------
+--Oracle & MySQL
+----------------------------------------------------------------------------------------------------------------
+    
+    
+
 -- innermost layer will give- 2, 77 because they directly report to 1
 -- second layer will give- 4 because he reports to 2
 -- outermost layer will give 7 because he reports to 4
